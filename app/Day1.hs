a94fb3d3b2c6a212d6b1af540fbb6631a29ebba2
chema.rodriguez <chema.rodriguez@cabify.com>
2025-12-01 12:14:59 +0100

    day1

--- app/Day1.hs
+++ app/Day1.hs
@@ -2,16 +2,23 @@ module Day1 (run) where
 
 import Combinators (both)
 import Control.Arrow ((&&&))
-import Data.List (sort, transpose)
-import Utils (chunk, combinations)
 
 run :: String -> (String, String)
 run = both show . (part1 &&& part2) . parse
 
-part1, part2 :: ([Int], [Int]) -> Int
-part1 _ = 42
-part2 _ = 42
+part1, part2 :: [Int] -> Int
+part1 = length . filter ((==0) . (`mod` 100))
+part2 =  sum . map count0s . (zip <*> tail)
 
-parse :: String -> ([Int], [Int])
-parse _ = ([], [])
--- parse = (head &&& last) . transpose . chunk 2 . map read . words
+count0s :: (Int, Int) -> Int
+count0s (n, n') =  abs $ offsetStart + offsetEnd + (div n' 100) - (div n 100)
+  where
+    offsetStart = if n `mod` 100 == 0 && (n' < n) then 1 else 0
+    offsetEnd = if n' `mod` 100 == 0 && (n' < n) then -1 else 0
+
+parse :: String -> [Int]
+parse = scanl (+) 50 . map parseLine . lines
+  where 
+    parseLine (sign : num) = parseSign sign * read num
+    parseSign 'R' = 1
+    parseSign 'L' = -1
